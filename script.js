784243e0755abcb637d58e4ead9b0a19bf439a6b
BoiBou58 <120332714+BoiBou58@users.noreply.github.com>
2024-09-20 21:30:25 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.dropdown').classList.toggle('show');
+});
+
+// Stars animation
+const starContainer = document.querySelector('.star-container');
+for (let i = 0; i < 100; i++) {
+    let star = document.createElement('div');
+    star.classList.add('star');
+    star.style.left = `${Math.random() * 100}vw`;
+    star.style.top = `${Math.random() * 100}vh`;
+    star.style.animationDuration = `${Math.random() * 10 + 5}s`;
+    starContainer.appendChild(star);
+}
